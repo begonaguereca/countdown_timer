d481d66bc7dd811ecb5bc115beacd3f9c95938e1
Begona Guereca <begonaguereca@gmail.com>
2018-06-27 14:02:24 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,16 +8,6 @@ app.use(bodyParser.json())
 app.use(express.static(__dirname + '/../react-client/dist'));
 
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
-});
-
 app.listen(3000, function() {
   console.log('listening on port 3000!');
 });
